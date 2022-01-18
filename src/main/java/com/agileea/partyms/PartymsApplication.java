c3014faa76707d6267b1d86d2fa1f8cf9b2b577f
Charles Edwards <Charles Edwards@AgileEa.com>
2022-01-18 12:03:22 +0200

    created project

---
+++ src/main/java/com/agileea/partyms/PartymsApplication.java
@@ -0,0 +1,13 @@
+package com.agileea.partyms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartymsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartymsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agileea/partyms/PartymsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agileea.partyms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartymsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
